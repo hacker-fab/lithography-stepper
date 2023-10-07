0a044f709eaf7c7b31da7c465ec3951604c459f7
Luca Garlati <lgarlati@andrew.cmu.edu>
2023-10-06 22:24:57 -0400

    I hate everything

--- scripts/litho.py
+++ scripts/litho.py
@@ -16,6 +16,12 @@ from litho_gui_lib import *
 # refactor this spaghetti
 # use the image.point function for rescaling
 # allow toggling pattern without re-importing
+# use max/min difference of grayscale converted flatfield image as suggestion 
+#   for the flatfield correction intensity in the debug widget
+# refactor alpha range handling
+# add advanced options feature to specify things like:
+# - downsampling target resolution for mask building
+# Better update detection system
 
 # declare root tk object
 root: Tk = Tk()
@@ -27,7 +33,8 @@ root: Tk = Tk()
 #   .2  Better Debug
 #   .3  Betterer Toggles
 #   .4  Better Thumbnails
-#   .5  
+#   .5  Main UI Controller
+#   .6
 
 root.title("Litho V4.4")
 
@@ -43,26 +50,16 @@ debug.grid(3, 0, colspan = 5)
 #   debug.info("Posterized pattern")
 
 
-# posterize toggle button
-posterize_toggle: Toggle = Toggle(root, debug = debug,
-                                  text=("Posterizing", "NOT Posterizing"))
-posterize_toggle.grid(1,0)
-
-# toggle alpha mask button
-flatfield_toggle: Toggle = Toggle(root, debug = debug,
-                                  text=("Using Flatfield", "NOT Using Flatfield"))
-flatfield_toggle.grid(1,1,colspan=2)
-
 # get projector dimentions
 def win_size() -> tuple[int,int]:
   return (proj.winfo_width(), proj.winfo_height())
 
 # set alpha range if specified, returns alpha range
 last_alpha_used: tuple[int,int] = (0,0)
-def alpha_range(new_range: tuple[int,int] = (0,0)) -> tuple[int,int]:
+def alpha_range(new_range: tuple[int,int] | None = None) -> tuple[int,int]:
   global last_alpha_used
   # check if query or assignment
-  if(new_range==(0,0)):
+  if(new_range == None):
     return last_alpha_used
   # assignment, validate input
   if not (new_range[0] >= 0 and
@@ -91,88 +88,27 @@ proj.grid_rowconfigure(0, weight=1)
 # projector variables
 thumbnail_size: tuple[int,int] = (160,90)
 # pattern_img:   Image.Image = Image.new('RGBA', thumbnail_size, (0,0,0,255))
-mask_img:      Image.Image = Image.new('RGBA', thumbnail_size, (0,0,0,0))
-alpha_channel: Image.Image
+# mask_img:      Image.Image = Image.new('RGBA', thumbnail_size, (0,0,0,0))
+# alpha_channel: Image.Image
 current_img:   Label = Label()
 
 # makes a photo thumbnail copy at specified size
-def auto_thumbnail(image: Image.Image, size: tuple[int,int] = thumbnail_size) -> ImageTk.PhotoImage:
-  thumb: Image.Image = image.copy()
-  thumb.thumbnail(size, Image.Resampling.LANCZOS)
-  return ImageTk.PhotoImage(thumb)
-
-# updates alpha channel to current settings
-def update_alpha_channel(force:bool = False):
-  global alpha_channel
-  # if we don't need to update, then we already know it's good (recursive assurance)
-  alphas: tuple[int,int] = (min_alpha.get(), max_alpha.get())
-  target_size: tuple[int,int] = fit_image(pattern_thumb.processed_img, win_size())
-  if(not force): 
-    debug.info("checking if mask needs updating...")
-  if (alpha_range() == alphas and 
-      mask_img.size == target_size and 
-      not force):
-    debug.info("skipped updating")
-    return
-  else:
-    debug.info("updating mask...")
-    global alpha_channel, prev_mask_button
-    #update alpha stuff
-    alpha_range(alphas)
-    alpha_channel = convert_to_alpha_channel(mask_img, new_scale=alpha_range(), target_size=target_size)
-    prev_mask_button.image = auto_thumbnail(alpha_channel)
-    prev_mask_button.config(image=prev_mask_button.image)
-    debug.info("finished building "+str(proj.winfo_width())+"x"+str(proj.winfo_height())+" with "+str(alpha_range())+" alpha channel mask")
-    
-
-# prev_pattern_button: Button = Button()
-# # set new pattern image
-# def set_pattern(query: bool = True):
-#   global pattern_img, prev_pattern_button
-#   if(query):
-#     # get image
-#     path: str = filedialog.askopenfilename(title ='Open')
-#     if(path == ''):
-#       debug.warn("Pattern import cancelled")
-#     else:
-#       debug.info("Pattern set to "+basename(path))
-#     # copy the image
-#     pattern_img = (Image.open(path)).copy()
-#     # resize to projector
-#     pattern_img = pattern_img.resize(fit_image(pattern_img, win_size=win_size()),
-#                                      Image.Resampling.LANCZOS)
-#   if(posterize_toggle.state):
-#     posterize_pattern()
-#   # delete previous button
-#   prev_pattern_button.destroy()
-#   # create thumbnail version
-#   img = auto_thumbnail(pattern_img)
-#   # display image with button
-#   button: Button = Button(
-#     root,
-#     image = img,
-#     text = "Pattern",
-#     compound = "top",
-#     command = set_pattern
-#     )
-#   button.image = img
-#   button.grid(
-#     row = 2,
-#     column = 0,
-#     sticky='nesw')
-#   prev_pattern_button = button
+# def auto_thumbnail(image: Image.Image, size: tuple[int,int] = thumbnail_size) -> ImageTk.PhotoImage:
+#   thumb: Image.Image = image.copy()
+#   thumb.thumbnail(size, Image.Resampling.LANCZOS)
+#   return ImageTk.PhotoImage(thumb)
 
 
-# will process the input image based on the current settings:
+# will process the input pattern based on the current settings:
 #   posterize
-#   flatfield
+#   size
 def pattern_processing_func(input_image: Image.Image) -> Image.Image:
   output_image: Image.Image = input_image.copy()
   if(posterize_toggle.state and output_image.mode != 'L'):
     output_image = posterize(output_image)
-  if(flatfield_toggle.state):
-    update_alpha_channel()
-    output_image.putalpha(alpha_channel)
+  if(input_image.size != fit_image(input_image, win_size())):
+    output_image = output_image.resize(win_size(), Image.Resampling.LANCZOS)
+    # size
   return output_image
 
 pattern_thumb: Thumbnail = Thumbnail(
@@ -183,9 +119,58 @@ pattern_thumb: Thumbnail = Thumbnail(
   func = pattern_processing_func)
 pattern_thumb.grid(2, 0)
 
+
+# effectively a wrapper for convert_to_alpha_channel
+def generate_flatfield(new_image: Image.Image) -> Image.Image:
+  # check if we need to update
+  alphas: tuple[int,int] = (min_alpha.get(), max_alpha.get())
+  target_size: tuple[int,int] = pattern_thumb.processed_img.size
+  debug.info("checking if mask needs updating...")
+  if (alpha_range() != alphas):
+    # alphas have changed
+    debug.info("mismatch alpha range, updating mask...")
+  elif (flatfield_thumb.processed_img.size != target_size):
+    # pattern image size has changed
+    debug.info("mismatch image size, updating mask...")
+  else:
+    debug.info("mask is up to date")
+    return flatfield_thumb.processed_img
+  #update alpha stuff
+  alpha_range(alphas)
+  alpha_channel = convert_to_alpha_channel(new_image, new_scale=alpha_range(),
+                                           target_size=target_size, downsample_target=540)
+  debug.info("finished building "+str(proj.winfo_width())+"x"+str(proj.winfo_height())+" mask")
+  return alpha_channel
+
+# effectively a wrapper for convert_to_alpha_channel
+def force_generate_flatfield(new_image: Image.Image) -> Image.Image:
+  # check if we need to update
+  alphas: tuple[int,int] = (min_alpha.get(), max_alpha.get())
+  target_size: tuple[int,int] = pattern_thumb.processed_img.size
+  debug.info("building mask...")
+  #update alpha stuff
+  alpha_range(alphas)
+  alpha_channel = convert_to_alpha_channel(new_image, new_scale=alpha_range(),
+                                           target_size=target_size, downsample_target=540)
+  debug.info("generating intensity guess...")
+  brightness: tuple[int,int] = get_brightness_range(flatfield_thumb.image.convert("L"))
+  debug.info("finished building "+str(proj.winfo_width())+"x"+str(proj.winfo_height())+" mask\n"+
+            "Recommended alpha range: ("+str(brightness[1]-brightness[0])+","+str(255)+")")
+  return alpha_channel
+
+
+flatfield_thumb: Thumbnail = Thumbnail(
+  root,
+  thumb_size = thumbnail_size,
+  text = "Flatfield Correction",
+  debug = debug,
+  func = generate_flatfield,
+  import_func=force_generate_flatfield)
+flatfield_thumb.grid(2, 1, colspan=2)
+
 red_focus_thumb: Thumbnail = Thumbnail(
   root,
-  thumb_size=thumbnail_size,
+  thumb_size = thumbnail_size,
   text = "Red Focus",
   debug = debug)
 red_focus_thumb.grid(2, 3)
@@ -197,39 +182,18 @@ UV_focus_thumb: Thumbnail = Thumbnail(
   debug=debug)
 UV_focus_thumb.grid(2,4)
 
-prev_mask_button: Button = Button()
-def set_mask(query: bool = True):
-  global mask_img, prev_mask_button, alpha_channel
-  if(query):
-    # get image
-    path: str = filedialog.askopenfilename(title ='Open')
-    if(path == ''):
-      debug.warn("flatfield import cancelled")
-    else:
-      debug.info("faltfield set to "+basename(path))
-    # save the image
-    mask_img = (Image.open(path)).copy()
-  # create alpha channel using mask
-  update_alpha_channel(force=True)
-  # delete previous button
-  prev_mask_button.destroy()
-  # create thumbnail version
-  img = auto_thumbnail(alpha_channel)
-  # display image with button
-  button: Button = Button(
-    root,
-    image = img,
-    text = "Correction Image",
-    compound = "top",
-    command = set_mask
-    )
-  button.image = img
-  button.grid(
-    row = 2,
-    column = 1,
-    columnspan = 2,
-    sticky='nesw')
-  prev_mask_button = button
+# posterize toggle button
+posterize_toggle: Toggle = Toggle(root, debug = debug,
+                                  text=("Posterizing", "NOT Posterizing"),
+                                  func=pattern_thumb.update)
+posterize_toggle.grid(1,0)
+
+# toggle alpha mask button
+flatfield_toggle: Toggle = Toggle(root, debug = debug,
+                                  text=("Using Flatfield", "NOT Using Flatfield"),
+                                  func=flatfield_thumb.update)
+flatfield_toggle.grid(1,1,colspan=2)
+
 
 # private method to show an image on the projection window
 max_alpha: Variable = IntVar()
@@ -240,7 +204,7 @@ def __show_img(input_image: Image.Image):
   # setup
   window_size: tuple[int,int] = (proj.winfo_width(), proj.winfo_height())
   img_copy: Image.Image = input_image.copy()
-  global current_img, alpha_channel
+  global current_img
   # resample if image isn't correct size
   if(img_copy.width != window_size[0] or
      img_copy.height != window_size[1]):
@@ -248,12 +212,14 @@ def __show_img(input_image: Image.Image):
     img_copy = img_copy.resize(fit_image(img_copy, window_size), Image.Resampling.LANCZOS)
   # apply alpha mask if enabled
   if(flatfield_toggle.state):
-    # check if alpha has changed
-    update_alpha_channel()
-    if(img_copy.size != alpha_channel.size):
-      debug.error("mismatch image sizes:\npattern: "+str(img_copy.size)+"\nmask: "+str(alpha_channel.size)+"\nproj: "+str((proj.winfo_width(),proj.winfo_height())))
+    flatfield_thumb.update()
+    if(img_copy.size != flatfield_thumb.processed_img.size):
+      debug.error("mismatch image sizes:\n"+
+                  "pattern: "+str(img_copy.size)+"\n"+
+                  "mask: "+str(flatfield_thumb.processed_img.size)+"\n"+
+                  "proj: "+str((proj.winfo_width(),proj.winfo_height())))
       assert(False)
-    img_copy.putalpha(alpha_channel)
+    img_copy.putalpha(flatfield_thumb.processed_img)
   # destroy currently displayed image
   current_img.destroy()
   # create new button
@@ -276,10 +242,9 @@ def begin_patterning():
     debug.error("duration = "+str(duration.get())+" < 0, aborting")
     return
   # prepare for patterning
-  global is_patterning
   pattern_button.configure(bg="black")
-  __show_img(pattern_thumb.processed_img)
   debug.info("Patterning for "+str(duration.get())+"ms...")
+  __show_img(pattern_thumb.processed_img)
   # begin
   root.update()
   sleep(duration.get() / 1000)
@@ -291,13 +256,13 @@ def begin_patterning():
 
 # show patterning image
 def show_focusing():
-  __show_img(red_focus_thumb.original_img)
+  __show_img(red_focus_thumb.image)
   debug.info("showing red focus pattern")
   root.update()
 
 # show uv focusing image
 def show_uv_focus():
-  __show_img(UV_focus_thumb.original_img)
+  __show_img(UV_focus_thumb.image)
   debug.info("showing uv focus pattern")
   root.update()
 
@@ -310,12 +275,9 @@ def clear_image():
 ### GUI ###
 ###########
 
-
-
 # setup GUI
 root.geometry("900x220")
-# don't make control window resizable
-# root.resizable(width = True, height = True)
+root.resizable(width = True, height = True)
 
 # weight all rows / cols
 root.grid_rowconfigure(0, weight=1)
@@ -331,8 +293,7 @@ root.grid_columnconfigure(4, weight=5)
   
 proj.update()
 # show default images
-# set_pattern(False)
-set_mask(False)
+# set_mask(False)
 
 # clear images button
 button: Button = Button(
