8157fbd268a1fb5df9598223b8733e7582178b67
Kent <jkentw2000@gmail.com>
2023-12-05 15:32:42 -0500

    flir camera stub

--- scripts/camera/flir/flir_camera.py
+++ scripts/camera/flir/flir_camera.py
@@ -1,3 +1,20 @@
-from ../camera_module import *
+# J. Kent Wirant
+# Hacker Fab
+# Flir Camera Module
 
-# ...
+from camera.camera_module import *
+# import [...]
+import time
+
+# for the time being, leave unimplemented in public repos
+class FlirCamera(CameraModule):
+    # camera description functions
+    def getDeviceInfo(self, parameterName):
+        match parameterName:
+            case 'vendor':
+                return "Flir"
+            case 'name':
+                return "Flir Camera"
+            case other:
+                return None
+
